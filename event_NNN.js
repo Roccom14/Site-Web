683decbea58bcca69aeaeebceb6f19ac0e9d6ca4
Rocco Ronzano <40248252+Roccom14@users.noreply.github.com>
2019-11-22 14:51:54 +0100

    [TEST] Tone

--- event_NNN.js
+++ event_NNN.js
@@ -5,7 +5,7 @@ audioElement.addEventListener('loadeddata', () => {
 })
 
 // Set the date we're counting down to
-var countDownDate = new Date("nov 22, 2019 14:50:00").getTime();
+var countDownDate = new Date("nov 22, 2019 14:53:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function(){
